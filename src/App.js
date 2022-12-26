0fe2dc22ba15bcc368942d44a6e83e58ed80587f
Vishakha Chaudhary <vishakhachaudhary@Vish-MacBook-Air.local>
2022-12-26 21:58:29 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+    <div class="navigation">
+      <Link to="/Home">Home</Link>
+      <Link to="/About">About</Link>
+      <Link to="/Contact">Contact</Link>
     </div>
+
+    <Routes>
+      <Route path='/' element={<Home/>} />
+
+      <Route path='/Contact' element={<Contact/>} />
+
+      <Route path='/About' element={<About/>} />
+    </Routes>
+   </Router>
   );
 }
 
 export default App;
+
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import bg from "../bg.jpeg"
+
+
+const Home = () => {
+    return(
+        <div>
+            <img src={bg} width={900} alt="loading..."/>
+        </div>
+    )
+}
+
+export default Home;
